56494ed248dc81c7759ab05e4ba7c322c83839fb
Lorenzo <lorenzo.fino@studenti.unicam.it>
2026-04-01 10:30:00 +0200

    init: aggiunta classe principale e test base

---
+++ src/main/java/unicam/hackhub/HackHubApplication.java
@@ -0,0 +1,15 @@
+package unicam.hackhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point of the HackHub application.
+ */
+@SpringBootApplication
+public class HackHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackHubApplication.class, args);
+    }
+}
